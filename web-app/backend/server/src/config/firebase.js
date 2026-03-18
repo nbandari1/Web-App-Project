44fe33c37e4479e5f9299c753ca8e61e8ad42911
RDT01 <ryan.tersigni@gmail.com>
2026-03-18 21:23:00 +0000

    Add backend setup

---
+++ web-app/backend/server/src/config/firebase.js
@@ -0,0 +1,14 @@
+const admin = require("firebase-admin");
+require("dotenv").config();
+
+const serviceAccount = require("../../serviceAccountKey.json");
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
+
+const db = admin.firestore();
+
+module.exports = { admin, db };
