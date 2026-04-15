cb6d601db8f57d38e1d11bb07af9c7221a068915
nbandari1 <133066486+nbandari1@users.noreply.github.com>
2026-04-15 13:05:51 -0400

    Update firebase.ts

--- web-app/backend/server/src/config/firebase.ts
+++ web-app/backend/server/src/config/firebase.ts
@@ -3,17 +3,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const serviceAccountPath = process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH;
-
-if (!serviceAccountPath) {
-  throw new Error("Missing Firebase service account key path.");
-}
-
-const serviceAccount = require(serviceAccountPath);
-
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID!,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+    }),
   });
 }
 
